b21db1cb9706afd200d8302eb2983f68f85e0ff7
sabysp <sabysp@aim.com>
2018-09-08 20:10:20 -0700

    Completed the Module
    completed

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,20 +41,22 @@ namespace GradeBook.UserInterfaces
             var name = parts[1];
 	        var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
-	        if (type=="standard")
+
+	        if (type == "standard")
 	        {
-				gradeBook = new StandardGradeBook(name);
+		        gradeBook = new StandardGradeBook(name);
 	        }
-			else if (type == "ranked")
+	        else if (type == "ranked")
 	        {
 				gradeBook=new RankedGradeBook(name);
 	        }
 	        else
 	        {
-		        Console.WriteLine("{0} is not a supported type of gradebook, please try again",type);
+		        Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
 		        return;
 	        }
-			Console.WriteLine("Created gradebook {0}.", name);
+
+	        Console.WriteLine("Created gradebook {0} of type {1}", name, type);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
